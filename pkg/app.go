3d264d6e7ab8405f2af68897f41c7904caf8e574
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-11 10:21:29 +0200

    feat: :sparkles: Connect to redis

--- pkg/app.go
+++ pkg/app.go
@@ -1,15 +1,30 @@
 package app
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
 	route "ideanest-task/pkg/api/routes"
 	database "ideanest-task/pkg/database/mongodb"
+	"ideanest-task/pkg/utils"
 )
 
-func RunApp() {
+
+func RunApp(ctx context.Context) {
+
+	// Initialize Redis connection
+	redisClient := utils.NewRedisClient()
+
 	// Initialize MongoDB connection
 	database.DB = database.ConnectDB()
+
+	// Ping Redis
+	err := redisClient.Ping(ctx).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Set up Gin router
 	router := gin.Default()
