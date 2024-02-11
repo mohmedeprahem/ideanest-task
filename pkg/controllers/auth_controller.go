770606808303afa17cbbeb328fe4696754090b91
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-11 10:20:29 +0200

    feat: :sparkles: Add SignInRequest Struct with SignIn method

--- pkg/controllers/auth_controller.go
+++ pkg/controllers/auth_controller.go
@@ -4,6 +4,8 @@ import (
 	"ideanest-task/pkg/database/mongodb/models"
 	"ideanest-task/pkg/database/mongodb/repository"
 	"ideanest-task/pkg/utils"
+	"log"
+	"time"
 
 	"net/http"
 
@@ -15,6 +17,12 @@ import (
 type UserController struct {
 	userRepository *repository.UserRepository
 }
+
+type SignInRequest struct {
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
 
 var validate = validator.New()
 
@@ -60,4 +68,56 @@ func (c *UserController) SignUp(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "user created successfully"})
+}
+
+func (c *UserController) SignIn(ctx *gin.Context) {
+	// Get user data from request body
+	var input  SignInRequest
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	// Check if user exists
+	user, err := c.userRepository.FindByEmail(input.Email)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid email or password"})
+		return
+	}
+
+
+	// User exists, check password
+	if err := utils.CheckPassword(user.Password, input.Password); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid email or password"})
+		return
+	}
+
+
+	// Generate access token
+	accessToken, err := utils.GenerateAccessToken(user.ID.Hex())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+
+	// Generate refresh token
+	refreshToken, err := utils.GenerateRefreshToken(user.ID.Hex())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+	}
+
+	// Save refresh token in redis
+	err = utils.SetRedisValue(refreshToken, true, 7 * 24 * time.Hour)
+	if err != nil {
+		log.Println(err)
+	    ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+	    return
+	}
+
+	// Return access token and refresh token
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "login successful",
+		"access_token": accessToken,
+		"refresh_token": refreshToken,
+	})
 }
