db93007de853f265eb8fc6ab3007fd00a5f5970f
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-11 05:04:07 +0200

    feat: :sparkles: Add auth routes file with signup POST api

---
+++ pkg/api/routes/auth_route.go
@@ -0,0 +1,13 @@
+package route
+
+import (
+	"ideanest-task/pkg/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoute(router *gin.Engine) {
+    authController := controllers.NewAuthController()
+
+    router.POST("/signup", authController.Create)
+}
