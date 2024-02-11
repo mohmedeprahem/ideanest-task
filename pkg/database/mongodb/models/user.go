e133924bc7f700b61d3897b135b96d9085d56280
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-11 04:58:46 +0200

    feat: :sparkles: Add validation in 'user' struct model

--- pkg/database/mongodb/models/user.go
+++ pkg/database/mongodb/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Name     string             `json:"name" bson:"name"`
-	Email    string             `json:"email" bson:"email"`
-	Password string             `json:"password" bson:"password"`
+	Name     string             `json:"name" bson:"name" validate:"required"`
+	Email    string             `json:"email" bson:"email" validate:"required,email"`
+	Password string             `json:"password" bson:"password"  validate:"required,min=8"`
 }
