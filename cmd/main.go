bec2dc688ed99061407d5e7c93bba97262861df6
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-11 05:05:20 +0200

    refactor: :recycle: Wrapped connect to server in app file and import router package

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	app "ideanest-task/pkg"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	app.Run()
-	r := gin.Default()
-
-	r.Run()
+	app.RunApp()
 }
--- pkg/app.go
+++ pkg/app.go
@@ -1,9 +1,22 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
+
+	route "ideanest-task/pkg/api/routes"
 	database "ideanest-task/pkg/database/mongodb"
 )
 
-func Run() {
-	database.ConnectDB()
+func RunApp() {
+	// Initialize MongoDB connection
+	database.DB = database.ConnectDB()
+
+	// Set up Gin router
+	router := gin.Default()
+
+	// Set up routes
+	route.AuthRoute(router)
+
+	// Start server
+	router.Run(":8080")
 }
